3e4d71fcd5aa71bdf6d7b18bde7b831a6a139eaf
thor.lee <yishh.lee@gmail.com>
2012-07-11 16:11:07 +0800

    修改版本号。增加MDC支持

--- logback.mongodb/src/main/java/logback/mongodb/MongoDBAppender.java
+++ logback.mongodb/src/main/java/logback/mongodb/MongoDBAppender.java
@@ -1,11 +1,13 @@
 package logback.mongodb;
 
 import java.util.Date;
+import java.util.Map;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.WriteResult;
 
 /**
  * @author Christian Trutz
@@ -22,7 +24,14 @@ public class MongoDBAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
 		logEntry.append("thread", eventObject.getThreadName());
 		logEntry.append("timestamp", new Date(eventObject.getTimeStamp()));
 		logEntry.append("level", eventObject.getLevel().toString());
+        Map<String, String> data = eventObject.getMDCPropertyMap();
+        for(String key:data.keySet()){
+            logEntry.append(key, data.get(key));
+        }
 		connectionSource.getDBCollection().insert(logEntry);
+//        System.out.println(result.getError());
+
+//        connectionSource.getDBCollection()
 	}
 
 	public void setConnectionSource(MongoDBConnectionSource connectionSource) {
